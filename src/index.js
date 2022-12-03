d9aef9546ada07bc292c76cb58ce688f465c7cc1
vivianne <viviannjoroge91@gmail.com>
2022-12-03 18:37:38 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener('submit',(e) => {
+    console.log(e)
+  })
 });
+//Create a to do list
+function createTodoList(todo){
+  let li = document.createElement('li');
+  let btn = document.createElement('button');
+  //add an event listner for handling the todo list delete
+  btn.addEventListener('click',handleDelete)
+  //assign text to the element
+  p.textContent = todo
+  btn.textContent = 'x';
+  //choose a parent element and append p to it
+  let parent = document.getElementById('tasks')
+  parent.appendChild(li)
+  p.appendChild(btn);
+  //create a function to delete done todo list items
+  function handleDelete(e){
+    return e.target.parentNode.remove()
+  }
+}
